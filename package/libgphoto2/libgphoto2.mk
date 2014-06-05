a1238c912cbaa64e1165fabf2d233ac028f92919
Daniel Dyer <danielrd@mac.com>
2014-06-05 01:07:32 -0700

    Removed unnecessary options from gphoto2 and libgphoto2

--- package/libgphoto2/libgphoto2.mk
+++ package/libgphoto2/libgphoto2.mk
@@ -8,7 +8,6 @@ LIBGPHOTO2_VERSION = 2.5.4
 LIBGPHOTO2_SOURCE = libgphoto2-$(LIBGPHOTO2_VERSION).tar.gz
 LIBGPHOTO2_SITE = http://downloads.sourceforge.net/project/gphoto/libgphoto/$(LIBGPHOTO2_VERSION)
 LIBGPHOTO2_INSTALL_STAGING = YES
-LIBGPHOTO2_CONF_OPT = --disable-docs
 LIBGPHOTO2_DEPENDENCIES = host-pkgconf libtool libexif libusb
 
 $(eval $(autotools-package))
