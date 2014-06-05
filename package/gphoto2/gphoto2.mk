507b68c44ca81b4e18e5b6c2750f0e0b8d3c6e06
Daniel Dyer <danielrd@mac.com>
2014-06-05 01:04:30 -0700

    Set source URI for gphoto2 and libgphoto2 to be not mirror-specific

--- package/gphoto2/gphoto2.mk
+++ package/gphoto2/gphoto2.mk
@@ -6,7 +6,7 @@
 
 GPHOTO2_VERSION = 2.5.4
 GPHOTO2_SOURCE = gphoto2-$(GPHOTO2_VERSION).tar.gz
-GPHOTO2_SITE = http://jaist.dl.sourceforge.net/project/gphoto/gphoto/$(GPHOTO2_VERSION)
+GPHOTO2_SITE = http://downloads.sourceforge.net/project/gphoto/gphoto/$(GPHOTO2_VERSION)
 GPHOTO2_INSTALL_STAGING = YES
 GPHOTO2_CONF_OPT = --disable-docs
 GPHOTO2_DEPENDENCIES = libgphoto2 libexif popt
