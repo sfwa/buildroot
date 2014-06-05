d9bfd684857bde0b0aed3d7b601aa978339cb3f1
Daniel Dyer <danielrd@mac.com>
2014-06-05 00:36:07 -0700

    Added libgphoto2 package

---
+++ package/libgphoto2/libgphoto2.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# libgphoto2
+#
+################################################################################
+
+LIBGPHOTO2_VERSION = 2.5.4
+LIBGPHOTO2_SOURCE = libgphoto2-$(LIBGPHOTO2_VERSION).tar.gz
+LIBGPHOTO2_SITE = http://ncu.dl.sourceforge.net/project/gphoto/libgphoto/$(LIBGPHOTO2_VERSION)
+LIBGPHOTO2_INSTALL_STAGING = YES
+LIBGPHOTO2_CONF_OPT = --disable-docs
+LIBGPHOTO2_DEPENDENCIES = host-pkgconf libtool libexif libusb
+
+$(eval $(autotools-package))
