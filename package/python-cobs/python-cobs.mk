ea6467f55256b22b304a3704efac86b8ff356791
Daniel Dyer <danielrd@mac.com>
2014-08-28 04:47:03 -0700

    Changed setup type for cobs and enum34 to 'distutils'

--- package/python-cobs/python-cobs.mk
+++ package/python-cobs/python-cobs.mk
@@ -7,6 +7,6 @@
 PYTHON_COBS_VERSION = 1.0.0
 PYTHON_COBS_SOURCE = cobs-$(PYTHON_COBS_VERSION).tar.gz
 PYTHON_COBS_SITE = https://pypi.python.org/packages/source/c/cobs
-PYTHON_COBS_SETUP_TYPE = setuptools
+PYTHON_COBS_SETUP_TYPE = distutils
 
 $(eval $(python-package))
--- package/python-enum34/python-enum34.mk
+++ package/python-enum34/python-enum34.mk
@@ -7,6 +7,6 @@
 PYTHON_ENUM34_VERSION = 1.0
 PYTHON_ENUM34_SOURCE = enum34-$(PYTHON_ENUM34_VERSION).tar.gz
 PYTHON_ENUM34_SITE = https://pypi.python.org/packages/source/e/enum34
-PYTHON_ENUM34_SETUP_TYPE = setuptools
+PYTHON_ENUM34_SETUP_TYPE = distutils
 
 $(eval $(python-package))
