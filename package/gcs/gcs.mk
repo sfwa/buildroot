88bc9673b99d767f765fa276fd57af570630fbeb
Daniel Dyer <danielrd@mac.com>
2014-09-10 05:12:18 -0700

    Added setuptools and updated gcs

--- package/gcs/gcs.mk
+++ package/gcs/gcs.mk
@@ -4,8 +4,15 @@
 #
 ################################################################################
 
-GCS_VERSION = ccd73ae860a55638d9b69b042534873534659616
+GCS_VERSION = 81d5f85ef4ca6c93234f83be153c9a0191d4984b
 GCS_SITE = git://github.com/sfwa/gcs.git
 GCS_SETUP_TYPE = setuptools
+
+define GCS_INSTALL_HEIGHTMAPS
+    (cd $(@D); mkdir -p $(TARGET_DIR)/usr/share/heightmaps; \
+     cp data/* $(TARGET_DIR)/usr/share/heightmaps)
+endef
+
+GCS_POST_INSTALL_TARGET_HOOKS += GCS_INSTALL_HEIGHTMAPS
 
 $(eval $(python-package))
