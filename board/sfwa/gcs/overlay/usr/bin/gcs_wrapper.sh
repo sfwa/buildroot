2dfa07ec89f591936572c3b54a950899f4ba6e12
Daniel Dyer <danielrd@mac.com>
2014-09-10 08:33:11 -0700

    Updated gcs_wrapper arguments

--- board/sfwa/gcs/overlay/usr/bin/gcs_wrapper.sh
+++ board/sfwa/gcs/overlay/usr/bin/gcs_wrapper.sh
@@ -3,6 +3,6 @@
 
 while [ 1 ]
 do
-    start-stop-daemon -x gcs-server -p /var/run/gcs-server.pid -Sbm -- --debug --verbose -p 8080 --nmea /dev/ttyO2 --heightmap /usr/share/heightmaps/S38E145.hgt /dev/ttyO1 /dev/ttyO5 /root/mission.conf
+    start-stop-daemon -S -b -a gcs-server -p /var/run/gcs-server.pid -m -- --debug --verbose -p 8080 --nmea /dev/ttyO2 --heightmap /usr/share/heightmaps/S38E145.hgt /dev/ttyO1 /dev/ttyO5 /root/mission.conf
     sleep 1
 done
